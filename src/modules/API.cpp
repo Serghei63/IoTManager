56640848c48d6edcfbc906b111fe207ecc5334a1
Serghei63 <you@example.com>
2026-08-01 14:22:56 +0300

    Перевод не основных модулей в false

--- src/modules/API.cpp
+++ src/modules/API.cpp
@@ -1,13 +1,23 @@
 #include "ESPConfiguration.h"
 
+void* getAPI_Cron(String subtype, String params);
 void* getAPI_Timer(String subtype, String params);
-void* getAPI_BL0937(String subtype, String params);
-void* getAPI_BL0942(String subtype, String params);
+void* getAPI_UpdateServer(String subtype, String params);
+void* getAPI_Variable(String subtype, String params);
+void* getAPI_VButton(String subtype, String params);
+void* getAPI_ButtonIn(String subtype, String params);
+void* getAPI_ButtonOut(String subtype, String params);
+void* getAPI_TelegramLT(String subtype, String params);
 
 void* getAPI(String subtype, String params) {
-void* tmpAPI;
-if ((tmpAPI = getAPI_Timer(subtype, params)) != nullptr) return tmpAPI;
-if ((tmpAPI = getAPI_BL0937(subtype, params)) != nullptr) return tmpAPI;
-if ((tmpAPI = getAPI_BL0942(subtype, params)) != nullptr) return tmpAPI;
-return nullptr;
+void* tmpAPI; void* foundAPI = nullptr;
+if ((tmpAPI = getAPI_Cron(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_Timer(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_UpdateServer(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_Variable(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_VButton(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_ButtonIn(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_ButtonOut(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_TelegramLT(subtype, params)) != nullptr) foundAPI = tmpAPI;
+return foundAPI;
 }
