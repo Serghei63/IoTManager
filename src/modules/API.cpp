ca8ca8ba708e2c9caecee23127ff4e0aded6dc72
Serghei63 <krasnikov.s.v@gmail.com>
2026-08-04 08:52:19 +0300

    Конфиг для радио

--- src/modules/API.cpp
+++ src/modules/API.cpp
@@ -1,27 +1,25 @@
 #include "ESPConfiguration.h"
 
 void* getAPI_Cron(String subtype, String params);
-void* getAPI_LogSd(String subtype, String params);
 void* getAPI_Timer(String subtype, String params);
 void* getAPI_UpdateServer(String subtype, String params);
 void* getAPI_Variable(String subtype, String params);
 void* getAPI_VButton(String subtype, String params);
 void* getAPI_ButtonIn(String subtype, String params);
 void* getAPI_ButtonOut(String subtype, String params);
-void* getAPI_SDcard(String subtype, String params);
+void* getAPI_Radio1334(String subtype, String params);
 void* getAPI_TelegramLT(String subtype, String params);
 
 void* getAPI(String subtype, String params) {
 void* tmpAPI; void* foundAPI = nullptr;
 if ((tmpAPI = getAPI_Cron(subtype, params)) != nullptr) foundAPI = tmpAPI;
-if ((tmpAPI = getAPI_LogSd(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_Timer(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_UpdateServer(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_Variable(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_VButton(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_ButtonIn(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_ButtonOut(subtype, params)) != nullptr) foundAPI = tmpAPI;
-if ((tmpAPI = getAPI_SDcard(subtype, params)) != nullptr) foundAPI = tmpAPI;
+if ((tmpAPI = getAPI_Radio1334(subtype, params)) != nullptr) foundAPI = tmpAPI;
 if ((tmpAPI = getAPI_TelegramLT(subtype, params)) != nullptr) foundAPI = tmpAPI;
 return foundAPI;
 }
