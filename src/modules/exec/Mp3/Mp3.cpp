67115f11f6bd832c64fed942b2997ea9f2ef097a
Serghei63 <krasnikov.s.v@gmail.com>
2026-08-07 12:11:42 +0300

    Обновление MP3 плеера

--- src/modules/exec/Mp3/Mp3.cpp
+++ src/modules/exec/Mp3/Mp3.cpp
@@ -1,15 +1,16 @@
-/*
 #include "Global.h"
 #include "classes/IoTItem.h"
 
 #include "SoftwareSerial.h"               // Подключаем библиотеку SoftwareSerial
-#include "DFRobotDFPlayerMini.h"            // Подключаем библиотеку DFPlayerMini_Fast
-
+#include "DFRobotDFPlayerMini.h"           // Подключаем библиотеку DFPlayerMini
 
 class Mp3 : public IoTItem {
    private:
     SoftwareSerial* mySerial;
     DFRobotDFPlayerMini* myMP3;
+    
+    int _currentTrack;
+    int _lastState;
 
    public:
     Mp3(String parameters): IoTItem(parameters) {
@@ -24,6 +25,9 @@ class Mp3 : public IoTItem {
 
         jsonRead(parameters, "volume", volumetmp);
 
+        _currentTrack = -1;
+        _lastState = -1;
+
         if (mySerial) {
             mySerial->begin(9600);
             myMP3 = new DFRobotDFPlayerMini();
@@ -31,251 +35,147 @@ class Mp3 : public IoTItem {
         if (myMP3) {
             myMP3->begin(*mySerial);
             myMP3->volume(volumetmp);
+            SerialPrint("I", "MP3", "MP3 module initialized, RX: " + String(pinRx) + ", TX: " + String(pinTx));
         } 
 
         value.isDecimal = false;    // значение объекта всегда будет строка
     }
 
-    void doByInterval() {
-        if (myMP3 && myMP3->available()) {
-            switch (myMP3->readType()) {
-                case TimeOut:
-                value.valS = F("Time Out!");
-                break;
-                case WrongStack:
-                value.valS = F("Stack Wrong!");
-                break;
-                case DFPlayerCardInserted:
-                value.valS = F("Card Inserted!");
-                break;
-                case DFPlayerCardRemoved:
-                value.valS = F("Card Removed!");
-                break;
-                case DFPlayerCardOnline:
-                value.valS = F("Card Online!");
-                break;
-                case DFPlayerPlayFinished:
+    void doByInterval() override {
+
+if (!myMP3) return;
+
+    // 1. Асинхронные события от плеера (когда чип сам шлет информацию)
+    if (myMP3->available()) {
+        switch (myMP3->readType()) {
+            case DFPlayerPlayFinished:
                 value.valS = F("Play Finished!");
+                regEvent(value.valS, F("Mp3"));
+                _lastState = -1; 
+                return; // Сразу выходим, давая веб-интерфейсу отобразить окончание
+            case DFPlayerError:
+                value.valS = F("Error!");
+                regEvent(value.valS, F("Mp3"));
+                return;
+            default:
                 break;
-                case DFPlayerError:
-                    switch (myMP3->read()) {
-                        case Busy:
-                        value.valS = F("Card not found");
-                        break;
-                        case Sleeping:
-                        value.valS = F("Sleeping");
-                        break;
-                        case SerialWrongStack:
-                        value.valS = F("Get Wrong Stack");
-                        break;
-                        case CheckSumNotMatch:
-                        value.valS = F("Check Sum Not Match");
-                        break;
-                        case FileIndexOut:
-                        value.valS = F("File Index Out of Bound");
-                        break;
-                        case FileMismatch:
-                        value.valS = F("Cannot Find File");
-                        break;
-                        case Advertise:
-                        value.valS = F("In Advertise");
-                        break;
-                        default:
-                        break;
-                    }
-                break;
-                default:
-                break;
-            }
         }
     }
 
-    IoTValue execute(String command, std::vector<IoTValue> &param) {
-        // реакция на вызов команды модуля из сценария
-        // String command - имя команды после ID. (ID.Команда())
-        // param - вектор ("массив") значений параметров переданных вместе с командой: ID.Команда("пар1", 22, 33) -> param[0].ValS = "пар1", param[1].ValD = 22
+    // Переменные для поочередного опроса
+    static byte queryStep = 0;
+    static int tempState = -1;
+    static int tempTrack = -1;
 
-        if (myMP3) {
-            if (command == "enableLoop") { 
-                myMP3->enableLoop();
-            } else if (command == "disableLoop") { 
-                myMP3->disableLoop();
-            } else if (command == "randomAll") { 
-                myMP3->randomAll();
-            } else if (command == "stop") { 
-                myMP3->stop();
-            } else if (command == "volume") { 
-                if (param.size()) {
-                    myMP3->volume(param[0].valD);
-                }
-            } else if (command == "playFolder") { 
-                if (param.size()) {
-                    myMP3->playFolder(param[0].valD, param[1].valD);    // (folderNum, fileNum)
-                }
-            } else if (command == "play") { 
-                myMP3->play(1);  //Play the first mp3
-            } else if (command == "next") { 
-                myMP3->next();  //Play next mp3
-            } else if (command == "previous") { 
-                myMP3->previous();  //Play previous mp3
-            }
+    queryStep++;
+
+    // Шаг 1: Опрашиваем ТОЛЬКО статус
+    if (queryStep == 2 || _lastState == -1) {
+        int stateResult = myMP3->readState();
+        if (stateResult != -1) { // Игнорируем ошибки таймаута UART
+            tempState = stateResult;
+        }
+    }
+    // Шаг 2: В следующий интервал опрашиваем ТОЛЬКО номер трека
+    else if (queryStep >= 4) {
+        queryStep = 0; // Сброс счетчика шагов
+        int trackResult = myMP3->readCurrentFileNumber();
+        if (trackResult != -1) { // Игнорируем ошибки таймаута UART
+            tempTrack = trackResult;
+        }
+    }
+
+    // Обработка результатов, если они валидны и изменились
+    if ((tempState != _lastState || tempTrack != _currentTrack) && tempState != -1) {
+        _lastState = tempState;
+        _currentTrack = tempTrack;
+
+        String statusStr = "";
+
+        if (_lastState == 1 || _lastState == 513) {
+            statusStr = "Playing #" + String(_currentTrack > 0 ? _currentTrack : 1);
+        } 
+        else if (_lastState == 2 || _lastState == 514) {
+            statusStr = "Paused #" + String(_currentTrack > 0 ? _currentTrack : 1);
+        }
+        else if (_lastState == 0 || _lastState == 512) {
+            statusStr = F("Stopped");
+        }
+        else {
+            // Если статус неизвестен, но это не -1, и у нас висит финиш — не трогаем
+            if (value.valS == F("Play Finished!")) return;
+            statusStr = "Status: " + String(_lastState);
         }
 
-        return {};  // команда поддерживает возвращаемое значения. Т.е. по итогу выполнения команды или общения с внешней системой, можно вернуть значение в сценарий для дальнейшей обработки
+        value.valS = statusStr;
+        regEvent(value.valS, F("Mp3"));
+    }
+    }
+
+    IoTValue execute(String command, std::vector<IoTValue> &param) override {
+        if (!myMP3) {
+            SerialPrint("E", "MP3", "Error: myMP3 is null!");
+            return {};
+        }
+
+        // Заставим doByInterval на следующем цикле принудительно обновить статус в веб
+        _lastState = -1; 
+
+        // 1. ИГРАТЬ ВСЁ С НАЧАЛА ПО ОЧЕРЕДИ
+        if (command == F("playAll")) {
+            myMP3->play(1);
+            myMP3->enableLoopAll();
+            SerialPrint("I", "MP3", "playAll() executed");
+        } 
+        // 2. ПОЛНЫЙ СТОП
+        else if (command == F("stop")) { 
+            myMP3->stop();
+            SerialPrint("I", "MP3", "stop() executed");
+        } 
+        // 3. ПАУЗА
+        else if (command == F("pause")) { 
+            myMP3->pause();
+            SerialPrint("I", "MP3", "pause() executed");
+        } 
+        // 4. ПРОДОЛЖЕНИЕ ИГРЫ
+        else if (command == F("continue")) { 
+            myMP3->start();
+            SerialPrint("I", "MP3", "continue() executed");
+        } 
+        // 5. СЛУЧАЙНЫЙ ВЫБОР ВСЕЙ КАРТЫ
+        else if (command == F("randomAll")) { 
+            myMP3->randomAll();
+            SerialPrint("I", "MP3", "randomAll() executed");
+        } 
+        // 6. ИГРАТЬ КОНКРЕТНУЮ ПАПКУ И ФАЙЛ
+        else if (command == F("playFolder")) { 
+            if (param.size() >= 2) {
+                int folder = param[0].valD;
+                int file = param[1].valD;
+                myMP3->playFolder(folder, file);
+                SerialPrint("I", "MP3", "playFolder() Folder: " + String(folder) + ", File: " + String(file));
+            }
+        } 
+        // 7. ИЗМЕНЕНИЕ ГРОМКОСТИ
+        else if (command == F("volume")) { 
+            if (param.size() >= 1) {
+                int vol = param[0].valD;
+                myMP3->volume(vol);
+                SerialPrint("I", "MP3", "volume() Set to: " + String(vol));
+            }
+        } 
+        // 8. СЛЕДУЮЩИЙ И ПРЕДЫДУЩИЙ
+        else if (command == F("next")) { 
+            myMP3->next();  
+        } 
+        else if (command == F("previous")) { 
+            myMP3->previous();  
+        }
+
+        return {};  
     }
 
     ~Mp3() {};
-};
-
-void* getAPI_Mp3(String subtype, String param) {
-    if (subtype == F("Mp3")) {
-        return new Mp3(param);
-    } else {
-        return nullptr;
-    }
-}
-*/
-#include "Global.h"
-#include "classes/IoTItem.h"
-
-#include "DFRobotDFPlayerMini.h"            // Подключаем библиотеку DFPlayerMini
-
-class Mp3 : public IoTItem {
-   private:
-    HardwareSerial* mySerial = nullptr;
-    DFRobotDFPlayerMini* myMP3 = nullptr;
-    bool isInitialised = false;
-
-   public:
-    Mp3(String parameters): IoTItem(parameters) {
-        String tmpstr;
-        int volumetmp = 15;
-        
-        jsonRead(parameters, "pins", tmpstr);
-        int pinRx = selectFromMarkerToMarker(tmpstr, ",", 0).toInt();  
-        int pinTx = selectFromMarkerToMarker(tmpstr, ",", 1).toInt();  
-        
-        jsonRead(parameters, "volume", volumetmp);
-
-        // Используем HardwareSerial2 (на ESP32 можно переназначить на любые свободные пины)
-        mySerial = new HardwareSerial(2);
-        myMP3 = new DFRobotDFPlayerMini();
-
-        if (mySerial && myMP3) {
-            // Baudrate, Config, RX, TX
-            mySerial->begin(9600, SERIAL_8N1, pinRx, pinTx);
-            
-            // Третий параметр false - отключаем долгие задержки при старте (isACK = false)
-            if (myMP3->begin(*mySerial, true, false)) {
-                myMP3->volume(volumetmp);
-                isInitialised = true;
-            } else {
-                Serial.println(F("[MP3] DFPlayer Mini not responding!"));
-            }
-        }
-
-        value.isDecimal = false;    // значение объекта всегда строка
-    }
-
-    void doByInterval() {
-        if (!isInitialised || !myMP3) return;
-
-        if (myMP3->available()) {
-            uint8_t type = myMP3->readType();
-            
-            switch (type) {
-                case TimeOut:
-                    value.valS = F("Time Out!");
-                    break;
-                case WrongStack:
-                    value.valS = F("Stack Wrong!");
-                    break;
-                case DFPlayerCardInserted:
-                    value.valS = F("Card Inserted!");
-                    break;
-                case DFPlayerCardRemoved:
-                    value.valS = F("Card Removed!");
-                    break;
-                case DFPlayerCardOnline:
-                    value.valS = F("Card Online!");
-                    break;
-                case DFPlayerPlayFinished:
-                    value.valS = F("Play Finished!");
-                    regEvent(value.valS, F("Mp3")); // Отправляем событие в сценарий
-                    break;
-                case DFPlayerError:
-                    switch (myMP3->read()) {
-                        case Busy:
-                            value.valS = F("Card not found");
-                            break;
-                        case Sleeping:
-                            value.valS = F("Sleeping");
-                            break;
-                        case SerialWrongStack:
-                            value.valS = F("Get Wrong Stack");
-                            break;
-                        case CheckSumNotMatch:
-                            value.valS = F("Check Sum Not Match");
-                            break;
-                        case FileIndexOut:
-                            value.valS = F("File Index Out of Bound");
-                            break;
-                        case FileMismatch:
-                            value.valS = F("Cannot Find File");
-                            break;
-                        case Advertise:
-                            value.valS = F("In Advertise");
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    IoTValue execute(String command, std::vector<IoTValue> &param) {
-        if (!myMP3) return {};
-
-        if (command == F("enableLoop")) { 
-            myMP3->enableLoop();
-        } else if (command == F("disableLoop")) { 
-            myMP3->disableLoop();
-        } else if (command == F("randomAll")) { 
-            myMP3->randomAll();
-        } else if (command == F("stop")) { 
-            myMP3->stop();
-        } else if (command == F("volume")) { 
-            if (param.size() >= 1) {
-                myMP3->volume(param[0].valD);
-            }
-        } else if (command == F("playFolder")) { 
-            if (param.size() >= 2) {
-                myMP3->playFolder(param[0].valD, param[1].valD);    // (folderNum, fileNum)
-            } else if (param.size() == 1) {
-                myMP3->playFolder(1, param[0].valD);                // По умолчанию папка 01
-            }
-        } else if (command == F("play")) { 
-            if (param.size() >= 1) {
-                myMP3->play(param[0].valD);                         // Сыграть трек N
-            } else {
-                myMP3->start();                                    // Возобновить воспроизведение
-            }
-        } else if (command == F("next")) { 
-            myMP3->next();
-        } else if (command == F("previous")) { 
-            myMP3->previous();
-        }
-
-        return {};
-    }
-
-    ~Mp3() {
-        if (myMP3) delete myMP3;
-        if (mySerial) delete mySerial;
-    }
 };
 
 void* getAPI_Mp3(String subtype, String param) {
