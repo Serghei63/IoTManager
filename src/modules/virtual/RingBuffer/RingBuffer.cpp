406d6adcc025a1e337a31aad079f1a2596bdacc0
Serghei63 <you@example.com>
2026-08-30 17:00:42 +0300

    Обновление вспомогательного модуля RingBuffer

--- src/modules/virtual/RingBuffer/RingBuffer.cpp
+++ src/modules/virtual/RingBuffer/RingBuffer.cpp
@@ -1,7 +1,6 @@
 #include "Global.h"
 #include "classes/IoTItem.h"
 
-
 class RingBuffer : public IoTItem {
    private:
     uint16_t** _buffer = nullptr; // Динамический двухмерный массив [channels][size]
@@ -10,6 +9,7 @@ class RingBuffer : public IoTItem {
     
     uint8_t _maxChannels = 0;
     uint16_t _bufferSize = 100;
+    int _debug = 0; // Для jsonRead(int&)
     
     std::vector<String> _targetIDs; // Список ID логируемых элементов
 
@@ -20,7 +20,10 @@ class RingBuffer : public IoTItem {
         jsonRead(parameters, "size", sizeStr);
         if (sizeStr.length()) _bufferSize = sizeStr.toInt();
 
-        // 2. Парсим список привязанных элементов (targets: "temp1,press1,pzem_p")
+        // 2. Парсим отладку (0/1)
+        jsonRead(parameters, "debug", _debug);
+
+        // 3. Парсим список привязанных элементов (targets: "temp1,press1,pzem_p")
         String targetsStr;
         jsonRead(parameters, "targets", targetsStr);
         
@@ -37,7 +40,7 @@ class RingBuffer : public IoTItem {
         _maxChannels = _targetIDs.size();
         if (_maxChannels == 0) _maxChannels = 4; // Резерв под 4 канала по умолчанию
 
-        // 3. Выделяем динамическую память под буферы
+        // 4. Выделяем динамическую память под буферы
         _buffer = new uint16_t*[_maxChannels];
         _head = new uint8_t[_maxChannels]();
         _count = new uint8_t[_maxChannels]();
@@ -54,17 +57,22 @@ class RingBuffer : public IoTItem {
         _buffer[ch][_head[ch]] = val;
         _head[ch] = (_head[ch] + 1) % _bufferSize;
         if (_count[ch] < _bufferSize) _count[ch]++;
-    }
 
-// Вспомогательная универсальная функция поиска элемента по ID через std::list
-IoTItem* findItem(const String& id) {
-    for (auto item : IoTItems) {
-        if (item && item->getID() == id) {
-            return item;
+        if (_debug) {
+            String chName = (ch < _targetIDs.size()) ? _targetIDs[ch] : String(ch);
+            SerialPrint("I", "RingBuffer", "Add val: " + String(val) + " -> " + chName + " (Count: " + String(_count[ch]) + ")");
         }
     }
-    return nullptr;
-}
+
+    // Вспомогательная универсальная функция поиска элемента по ID через std::list
+    IoTItem* findItem(const String& id) {
+        for (auto item : IoTItems) {
+            if (item && item->getID() == id) {
+                return item;
+            }
+        }
+        return nullptr;
+    }
 
     // Вызывается базовым классом IoTItem с интервалом "int" из config.json
     void doByInterval() override {
@@ -105,14 +113,70 @@ IoTItem* findItem(const String& id) {
     uint8_t getMaxChannels() {
         return _maxChannels;
     }
+    // Вызывается движком сценариев при выполнении logbuf.print() или при изменении значения
+    void setValue(const IoTValue& Value, bool isTrigger = true) override {
+        // Если прилетела команда в виде строки
+        String cmd = Value.valS;
+        
+        if (cmd == F("print")) {
+            printBuffer();
+        }
+        else if (cmd == F("save")) {
+            doByInterval();
+        }
+        else {
+            // Вызываем базовую обработку
+            IoTItem::setValue(Value, isTrigger);
+        }
+    }
 
-    // Обработка прямых команд из сценариев IoTmanager
-    IoTValue execute(String command, std::vector<IoTValue> &param) override {
-        if (command == "push" && param.size() >= 2) {
+    // Вывод содержимого буфера в веб-консоль и Serial
+    void printBuffer(int targetCh = 255) {
+        
+        SerialPrint("I", "RingBuffer", "========== DUMP ==========");
+        for (uint8_t ch = 0; ch < _maxChannels; ch++) {
+            if (targetCh != 255 && targetCh != ch) continue;
+
+            String targetID = (ch < _targetIDs.size()) ? _targetIDs[ch] : "ch_" + String(ch);
+            String out = "Ch " + String(ch) + " [" + targetID + "] (" + String(_count[ch]) + "/" + String(_bufferSize) + "): [ ";
+            
+            for (uint8_t i = 0; i < _count[ch]; i++) {
+                out += String(getPoint(ch, i));
+                if (i < _count[ch] - 1) out += ", ";
+            }
+            out += " ]";
+            SerialPrint("I", "RingBuffer", out);
+        }
+        SerialPrint("I", "RingBuffer", "==========================");
+    }
+
+// Обработка прямых C++ вызовов и команд с параметрами
+IoTValue execute(String command, std::vector<IoTValue> &param) override {
+        if (command == F("push") && param.size() >= 2) {
             push((uint8_t)param[0].valD, (uint16_t)param[1].valD);
         }
-        else if (command == "save") {
+        else if (command == F("save")) {
             doByInterval();
+        }
+        else if (command == F("print")) {
+            uint8_t targetCh = (param.size() > 0) ? (uint8_t)param[0].valD : 255;
+            printBuffer(targetCh);
+        }
+        // --- ДЛЯ ВЫГРУЗКИ В ЭКРАНЫ ---
+        else if (command == F("getCount") && param.size() > 0) {
+            uint8_t ch = (uint8_t)param[0].valD;
+            IoTValue res;
+            res.valD = getCount(ch);
+            res.isDecimal = true;
+            return res;
+        }
+        else if (command == F("getPoint") && param.size() >= 2) {
+            uint8_t ch = (uint8_t)param[0].valD;
+            uint8_t idx = (uint8_t)param[1].valD;
+            IoTValue res;
+            res.valD = getPoint(ch, idx);
+            res.isDecimal = true;
+            return res;
         }
         return {};
     }
